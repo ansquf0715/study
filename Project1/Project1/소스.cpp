9e2c59e690ae284b95d637fa72fb0fff891edcc1
ansquf0715 <ansquf0715@naver.com>
2021-03-24 01:22:10 +0900

    2-4-2

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,31 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#include <cstring>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
-	char name[10];
-	char copy[30] = "Name is ";
+	srand(time(NULL));
 
-	cout << "Enter your name : ";
-	cin >> name;
-
-	int length = 0;
-	length = strlen(name);
-	cout << "length : " << length << endl;
-
-	cout << strcat(copy, name) << endl;
-
-	cout << copy << endl;
-
-	cout << strcpy(copy, name) << endl;
-
-	cout << copy << endl;
-
-	cout << strcmp(copy, name) << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Random number : " << i << " : " << rand() % 100 << endl;
+	}
 
 	return 0;
 }
