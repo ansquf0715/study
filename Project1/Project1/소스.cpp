14a48eceaf040cdc81d704b843d32c7760247a83
ansquf0715 <ansquf0715@naver.com>
2021-03-24 01:03:13 +0900

    2-3

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,6 +1,38 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+typedef struct __Point
+{
+	int xpos;
+	int ypos;
+}Point;
+
+Point& PntAdder(const Point &p1, const Point &p2)
+{
+	Point *ans = new Point;
+	ans->xpos = p1.xpos + p2.xpos;
+	ans->ypos = p1.ypos + p2.ypos;
+
+	return *ans;
+}
 
 int main()
 {
-	printf("Hello world!");
+	Point *number1 = new Point;
+	number1->xpos = 3;
+	number1->ypos = 30;
+
+	Point *number2 = new Point;
+	number2->xpos = 70;
+	number2->ypos = 7;
+
+	Point &temp = PntAdder(*number1, *number2);
+	cout << " x: " << temp.xpos << " " << "y : " << temp.ypos << endl;
+
+	delete number1;
+	delete number2;
+	delete &temp;
+
+	return 0;
 }
