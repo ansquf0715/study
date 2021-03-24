84e9fae93aed81b9c982a2ff2b9d602efd14793d
ansquf0715 <ansquf0715@naver.com>
2021-03-24 23:54:42 +0900

    03-2-1

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -4,39 +4,68 @@
 
 using namespace std;
 
-struct Point
+class Calculator
 {
-	int xpos;
-	int ypos;
+private:
+	int Addcount;
+	int Divcount;
+	int Mincount;
+	int Mulcount;
 
-	void MovePos(int x, int y)
+public:
+	void Init()
 	{
-		xpos = xpos + x;
-		ypos = ypos + y;
+		Addcount = 0;
+		Divcount = 0;
+		Mincount = 0;
+		Mulcount = 0;
 	}
 
-	void AddPoint(const Point &pos)
+	double Add(double x, double y)
 	{
-		xpos += pos.xpos;
-		ypos += pos.ypos;
+		Addcount++;
+		return x + y;
 	}
 
-	void ShowPosition()
+	double Div(double x, double y)
 	{
-		cout << "[ " << xpos << " , " << ypos << " ] " << endl;
+		Divcount++;
+		return x / y;
+	}
+
+	double Min(double x, double y)
+	{
+		Mincount++;
+		return x - y;
+	}
+
+	double Mul(double x, double y)
+	{
+		Mulcount++;
+		return x * y;
+	}
+
+	void ShowOpCount()
+	{
+		cout << "Add : " << Addcount << endl;
+		cout << "Div : " << Divcount << endl;
+		cout << "Min : " << Mincount << endl;
+		cout << "Mul : " << Mulcount << endl;
 	}
 };
 
 int main()
 {
-	Point pos1 = { 12,4 };
-	Point pos2 = { 20,30 };
+	Calculator cal;
 
-	pos1.MovePos(-7, 10);
-	pos1.ShowPosition();
+	cal.Init();
+	
+	cout << "3.2 + 2.4 = " << cal.Add(3.2, 2.4) << endl;
+	cout << "3.5 / 1.7 = " << cal.Div(3.5, 1.7) << endl;
+	cout << "2.2 - 1.5 = " << cal.Min(2.2, 1.5) << endl;
+	cout << "4.9 / 1.2 = " << cal.Div(4.9, 1.2) << endl;
 
-	pos1.AddPoint(pos2);
-	pos1.ShowPosition();
+	cal.ShowOpCount();
 
 	return 0;
 }
