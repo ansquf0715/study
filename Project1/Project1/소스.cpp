384eccbd02a364d41a06adc6067f04370c94dabb
ansquf0715 <ansquf0715@naver.com>
2021-03-25 00:04:11 +0900

    03-2-2

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,71 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-class Calculator
+class Printer
 {
 private:
-	int Addcount;
-	int Divcount;
-	int Mincount;
-	int Mulcount;
-
+	char words[100];
 public:
-	void Init()
+	void SetString(const char* input)
 	{
-		Addcount = 0;
-		Divcount = 0;
-		Mincount = 0;
-		Mulcount = 0;
+		strcpy(words, input);
+	}
+	void ShowString()
+	{
+		cout << words << endl;
 	}
 
-	double Add(double x, double y)
-	{
-		Addcount++;
-		return x + y;
-	}
-
-	double Div(double x, double y)
-	{
-		Divcount++;
-		return x / y;
-	}
-
-	double Min(double x, double y)
-	{
-		Mincount++;
-		return x - y;
-	}
-
-	double Mul(double x, double y)
-	{
-		Mulcount++;
-		return x * y;
-	}
-
-	void ShowOpCount()
-	{
-		cout << "Add : " << Addcount << endl;
-		cout << "Div : " << Divcount << endl;
-		cout << "Min : " << Mincount << endl;
-		cout << "Mul : " << Mulcount << endl;
-	}
 };
 
 int main()
 {
-	Calculator cal;
+	Printer pnt;
 
-	cal.Init();
-	
-	cout << "3.2 + 2.4 = " << cal.Add(3.2, 2.4) << endl;
-	cout << "3.5 / 1.7 = " << cal.Div(3.5, 1.7) << endl;
-	cout << "2.2 - 1.5 = " << cal.Min(2.2, 1.5) << endl;
-	cout << "4.9 / 1.2 = " << cal.Div(4.9, 1.2) << endl;
+	pnt.SetString("Hello world!");
+	pnt.ShowString();
 
-	cal.ShowOpCount();
+	pnt.SetString("I love C++");
+	pnt.ShowString();
 
 	return 0;
 }
