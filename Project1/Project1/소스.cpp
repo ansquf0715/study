a13bcf337bd79c64fb14e43756bd2c17ec615c57
ansquf0715 <ansquf0715@naver.com>
2021-03-24 23:11:11 +0900

    03-1

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,19 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
 
 using namespace std;
 
+struct Point
+{
+	int xpos;
+	int ypos;
+
+	void MovePos(int x, int y)
+	{
+		xpos = xpos + x;
+		ypos = ypos + y;
+	}
+
+	void AddPoint(const Point &pos)
+	{
+		xpos += pos.xpos;
+		ypos += pos.ypos;
+	}
+
+	void ShowPosition()
+	{
+		cout << "[ " << xpos << " , " << ypos << " ] " << endl;
+	}
+};
+
 int main()
 {
-	srand(time(NULL));
+	Point pos1 = { 12,4 };
+	Point pos2 = { 20,30 };
 
-	for (int i = 0; i < 5; i++)
-	{
-		cout << "Random number : " << i << " : " << rand() % 100 << endl;
-	}
+	pos1.MovePos(-7, 10);
+	pos1.ShowPosition();
+
+	pos1.AddPoint(pos2);
+	pos1.ShowPosition();
 
 	return 0;
 }
